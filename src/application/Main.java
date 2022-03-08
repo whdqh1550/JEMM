e70780b41fa89375a4b1fce8a43ae7b367742f2a
Jongbo Im <whdqh1550@gmail.com>
2022-03-07 22:59:22 -0600

    testcommit

--- src/application/Main.java
+++ src/application/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
-			primaryStage.show();
+			primaryStage.show();//Test comment 
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
