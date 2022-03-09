142e8d3098728c198fdc12be8c6a3b6e4b6ed553
Jongbo Im <whdqh1550@gmail.com>
2022-03-09 14:19:47 -0600

    branch test

--- src/application/Main.java
+++ src/application/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
-			primaryStage.show();//Test comment 
+			primaryStage.show();//branch test by Jongbo 
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
