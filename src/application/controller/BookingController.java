06f94a253f2d2442ad407835b962da5b9f8dde32
mushroomclds <aldaymario25@gmail.com>
2022-04-06 16:22:07 -0500

    new stuff

--- src/application/controller/BookingController.java
+++ src/application/controller/BookingController.java
@@ -1,20 +1,29 @@
 package application.controller;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.ResourceBundle;
+
 import application.model.Data;
 
-public class BookingController {
+public class BookingController implements Initializable {
 
     @FXML
     private AnchorPane mainPane;
     @FXML
     private TextField phoneNumberField;
+    
+    @FXML
+    private ListView<String> listView;
 
     @FXML
     private TextField nameField;
@@ -22,11 +31,21 @@ public class BookingController {
     @FXML
     void ConfirmBookingPressed(ActionEvent event) throws Exception {
     	ArrayList<String> roomInfo = new ArrayList<String>();
+    	Data var = new Data();
+    	var.loadData();
     	String name=  nameField.getText().toString();
    	 	String phoneNumber= phoneNumberField.getText().toString();
    	 	roomInfo.add(name);
    	 	roomInfo.add(phoneNumber);	
-   	  	Data.addRoomInfo("15",  roomInfo); //call addItem function in model class
+   	 	String roomNum="";
+   	 	ObservableList listofItems=listView.getSelectionModel().getSelectedItems();
+   	 	for(Object item : listofItems){
+   	 		roomNum+=String.format("%s", (String)item);
+   	 	}
+   	 	System.out.println(roomNum);
+   	 	
+   	  	var.addRoomInfo(roomNum,  roomInfo); //call addItem function in model class
+//   	 	Data.loadData();
    	  	nameField.clear();
    	  	phoneNumberField.clear(); 
     }
@@ -35,5 +54,22 @@ public class BookingController {
     void CheckAvailabilityPressed(ActionEvent event) {
 
     }
-
+    
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1){	
+    	int[] a = new int[75];
+//    	for (int i = 1; i < 76; i++) {
+//    		a[i-1] = i;
+//    		System.out.println(a[i-1]);
+//    	}
+    	
+		listView.getItems().addAll("1","2","3","4","5", "51");
+		try {
+			Data.loadData();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		 //listview.getItems().addAll("Vogue1","Time2","Elle2","Fortune2");
+		 //listview.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+	}
 }
--- src/application/model/Data.java
+++ src/application/model/Data.java
@@ -1,47 +1,113 @@
 package application.model;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.stream.Stream;
 
 public class Data {
 	private static Map<String, ArrayList<String>> data = new HashMap<String, ArrayList<String>>(); //hashmap type map, takes string and arraylist of type string, to hold multiple values per key
-	private static Properties properties=new Properties();
-	private static HashMap<String, String> h1=new HashMap<String,String>();
-	
+//	private static Properties properties=new Properties();
+//	private static HashMap<String, String> h1=new HashMap<String,String>();
+	private static int csvCurrentLine = 10;
 	public static void addRoomInfo (String room, ArrayList<String> roomInfo) throws Exception{
-		data.put(room, roomInfo);
-//		h1.put("room", "this");
-		System.out.println(roomInfo);
-//		data.put(item,quantity);
-		properties.putAll(data);
-		System.out.println(data);
-//		File file=new File("data.properties");
-		File file = new File("data.properties");
-		try {
-			file.createNewFile();
-			if (file.exists()) {
-				System.out.println("Data file created as: " + file.getName());
-			}
-   		}
-		catch (IOException e) {
-	    	System.out.println("Data file error");
-	    	e.printStackTrace();
-	    }
 		
-		
-		FileOutputStream writer = new FileOutputStream(file,true);// fileoutput used for writing to files
-		System.out.println("f");
-		if (writer == null) {
-			System.out.println("ffffffffff");
+		if(data.containsKey(room)) {
+			System.out.println("room taken bye");
+			return;
 		}
-		properties.store(writer,null);//this gives error because there is list in data
-		System.out.println("ff");
-	}
-	
+		
+		data.put(room, roomInfo); //adding to data structure 
+//		h1.put("room", "this");
+		System.out.println("Room booked: " + room +"\tRoom information: "+ roomInfo);
+//		data.put(item,quantity);
+//		System.out.println("\n"+data);
+//		try (PrintWriter writer = new PrintWriter("src/data.csv")) { //adding to csv file for storage
+//
+//		      StringBuilder sb = new StringBuilder();
+//		      
+//		      String newLine = "";
+//		      for (int i = 0; i < csvCurrentLine; ++i) {
+//		    	  newLine += "\n";
+//		      }
+//		      sb.append(newLine); //use insert with given line index 
+////		      sb.insert(5, "yes");
+//		      sb.append(room); 
+//		      sb.append(',');
+//		      for (int i = 0; i < roomInfo.size(); ++i) {
+//		    	  sb.append(roomInfo.get(i));
+//		    	  sb.append(',');
+//		      }
+//		      sb.append('\n');
+////		      sb.append("1");
+////		      sb.append(','); //go next column
+////		      sb.append("Prashant Ghimire");
+////		      sb.append('\n');//go to next row
+//		      writer.write(sb.toString());
+//		      System.out.println("done!");
+//		    } catch (FileNotFoundException e) {
+//		      System.out.println(e.getMessage());
+//		    }
+		File dir = new File("src/data.csv");
+		String loc = dir.getCanonicalPath();
+ 
+		FileWriter fstream = new FileWriter(loc, true);
+		BufferedWriter out = new BufferedWriter(fstream);
+		String toCSV = "";
+		toCSV += room;
+		for (int i = 0; i < roomInfo.size(); ++i) {
+			toCSV += ","+roomInfo.get(i);
+		}	
+		out.write(toCSV);
+		out.newLine();
+		loadData();
+		out.close();
 
+	}	 	  
+	public static void loadData() throws IOException {
+//		Map<String, ArrayList<String>> data = new HashMap<String, ArrayList<String>>();
+		File file = new File("src/data.csv");
+		if (!(file.exists())) { //exception 
+			throw new IOException();
+		}
+		FileReader stream = new FileReader(file);
+		BufferedReader buffer = new BufferedReader(stream);
+		String line;
+		String room = "";
+		while((line = buffer.readLine()) != null) {
+			ArrayList<String> csvDataArray = new ArrayList<String>();
+			String [] csvLine = {}; //array to split the csv information 
+			csvLine = line.split(",");
+//			for (int i=0; i < csvLine.length; i++) {
+//				System.out.print(csvLine[i]);
+//			}
+//			System.out.println("           " +line);
+			room = csvLine[0];
+			for (int i = 1; i < csvLine.length; ++i) {
+				csvDataArray.add(csvLine[i]);
+			}
+			data.put(room, csvDataArray);
+//			csvCurrentLine++;
+		}
+		
+//		System.out.println("loaded Data: "+dataLoad);
+		Iterator iterator = data.entrySet().iterator();
+        while (iterator.hasNext()) {
+             Map.Entry me2 = (Map.Entry) iterator.next();
+          System.out.println("Room: "+me2.getKey() + "  RoomInfo: " + me2.getValue());
+        } 
+		buffer.close(); //close file 
+	}
 }
